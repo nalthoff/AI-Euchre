4255b6815579555499f8afb4faad58ce84d95538
Nick <nick.althoff@gmail.com>
2025-05-12 22:02:41 -0500

    scaffolded 3 components and the game service.

---
+++ src/app/new-game/new-game.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-game',
+  imports: [],
+  templateUrl: './new-game.component.html',
+  styleUrl: './new-game.component.scss'
+})
+export class NewGameComponent {
+
+}
